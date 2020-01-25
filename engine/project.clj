39b4056dc78fbdd1b6b144a4f6bf9c508ddc80de
andriikovalov <andrewsk@ukr.net>
2020-01-25 18:18:43 +0100

    Engine listening to game management queue

--- engine/project.clj
+++ engine/project.clj
@@ -1,7 +1,8 @@
 (defproject engine "0.1.0-SNAPSHOT"
   :description "Game engine"
   :url "https://gavr.games"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.novemberain/langohr "5.1.0"]]
   :main ^:skip-aot server.core
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
--- engine/src/clojure/server/core.clj
+++ engine/src/clojure/server/core.clj
@@ -1,10 +1,31 @@
 (ns server.core
-  (:gen-class))
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb]))
+
+(def ^{:const true}
+  default-exchange-name "")
+
+(def ^{:const true}
+  management-q-name "game.management")
+
+(defn management-handler
+  [ch {:keys [delivery-tag] :as meta} ^bytes payload]
+  (println "Received a message: " (String. payload "UTF-8") meta)
+  (lb/ack ch delivery-tag))
 
 
 (defn -main
   [& args]
-  (println "Started game engine server")
-  (println "TODO connect to rabbitmq and subscribe to game management queue")
-  (while true
-    (Thread/sleep 60000)))
+  (println "Game engine server starting")
+  (let [conn (rmq/connect)
+        ch (lch/open conn)]
+    (println "Connected to RabbitMQ. Channel id: " (.getChannelNumber ch))
+    (lq/declare ch management-q-name)
+    (lc/subscribe ch management-q-name management-handler)
+
+    (while true
+      (Thread/sleep 60000))))
