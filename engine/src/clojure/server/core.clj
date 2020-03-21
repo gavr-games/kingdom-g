f372096a2b5731445b422e5f956d68e6455745f4
andriikovalov <andrewsk@ukr.net>
2020-03-21 18:42:49 +0100

    RabbitMQ reading and writing for game commands and actions.

--- engine/src/clojure/server/core.clj
+++ engine/src/clojure/server/core.clj
@@ -1,22 +1,30 @@
 (ns server.core
   (:gen-class)
   (:require [engine.newgame :refer [create-game-shuffled-players]]
+            [engine.actions :as action]
             [langohr.core      :as rmq]
             [langohr.channel   :as lch]
             [langohr.queue     :as lq]
             [langohr.consumers :as lc]
             [langohr.basic     :as lb]
+            [langohr.exchange  :as le]
             [cheshire.core     :as json]))
 
 (def games
   "Map of game id to game data."
   (ref {}))
 
-(def ^{:const true}
-  default-exchange-name "")
+(def default-exchange-name "")
 
-(def ^{:const true}
-  management-q-name "game.management")
+(def management-q-name "game.management")
+
+(defn game-actions-q-name
+  [game-id]
+  (str "game.actions." game-id))
+
+(defn game-commands-exchange-name
+  [game-id]
+  (str "game.commands." game-id))
 
 (defn ex-logging
   "Wraps given function into try-catch which prints exceptions to stderr."
@@ -25,32 +33,86 @@
     (try
       (apply f args)
       (catch Exception ex
-        (.println *err* (str "Uncaught exception on" f ex))))))
+        (clojure.stacktrace/print-stack-trace ex)))))
 
 (defn clean-players
   "Extracts :id and :team fields from player list."
   [players]
   (map #(select-keys % [:id :team]) players))
 
-(defn process-game-creation
+(defn act! [g-id p action params]
+  (println "ACT!" g-id p action params)
+  (let [action (keyword action)]
+    (dosync
+     (let [g (@games g-id)
+           action-result (action/act g p action params)]
+       (if (keyword? action-result)
+         {:success false :error action-result}
+         (let [g-after action-result
+               new-commands (subvec (g-after :commands) (count (g :commands)))
+               over (= :over (g-after :status))]
+           (if over
+             (alter games dissoc g-id)
+             (alter games assoc g-id g-after))
+           {:success true :commands new-commands}))))))
+
+(defn send-game-message
+  [game-id ch routing-key message]
+  (let [ename (game-commands-exchange-name game-id)
+        message-json (json/encode message)]
+    (lb/publish ch ename routing-key message-json)))
+
+(defn process-game-action
+  [game-id ch action params]
+  (let [p (:p params)
+        action-result (act! game-id p action params)
+        success (:success action-result)
+        routing-key (if success "commands" "reply")]
+    (send-game-message game-id ch routing-key action-result)))
+
+(defn game-action-handler
+  [game-id ch message-meta ^bytes payload]
+  (lb/ack ch (:delivery-tag message-meta))
+  (let [message-str (String. payload "UTF-8")]
+    (println "Received a message for game" game-id ":"
+             message-str message-meta)
+    (let [message (json/decode message-str true)
+          action (:action message)
+          params (:parameters message)]
+      (case action
+        "get-game-data" (send-game-message game-id ch "reply" "{\"Not-implemented-yet\":0}")
+        (process-game-action game-id ch action params)))))
+
+(defn create-game-actions-handler
+  [ch game-id]
+  (let [actions-q-name (game-actions-q-name game-id)]
+    (lq/declare ch actions-q-name)
+    (lc/subscribe ch actions-q-name
+                  (ex-logging (partial game-action-handler game-id)))))
+
+(defn create-game-commands-exchange
+  [ch game-id]
+  (le/direct ch (game-commands-exchange-name game-id)))
+
+(defn process-game-creation!
   [ch game-data]
   (let [game-id (:id game-data)
         players (clean-players (:players game-data))
         game (create-game-shuffled-players players)]
-    (dosync (alter games assoc game-id game)))
-  ; TODO
-  )
+    (dosync (alter games assoc game-id game))
+    (create-game-commands-exchange ch game-id)
+    (create-game-actions-handler ch game-id)))
 
 (defn management-handler
-  [ch {:keys [delivery-tag] :as meta} ^bytes payload]
+  [ch {:keys [delivery-tag] :as message-meta} ^bytes payload]
   (lb/ack ch delivery-tag)
   (let [message-str (String. payload "UTF-8")]
-    (println "Received a message:" message-str meta)
+    (println "Received a message:" message-str message-meta)
     (let [message (json/decode message-str true)
           action (:action message)
           data (:body message)]
       (case action
-        "create_game" (process-game-creation ch data)
+        "create_game" (process-game-creation! ch data)
         (println "Unknown action" action)))))
 
 (defn start-game-server
@@ -60,7 +122,7 @@
   (Thread/setDefaultUncaughtExceptionHandler
    (reify Thread$UncaughtExceptionHandler
      (uncaughtException [_ thread ex]
-       (.println *err* (str "Uncaught exception on" (.getName thread) ex)))))
+       (clojure.stacktrace/print-stack-trace ex))))
   (println "Game engine server starting"
            (.toString (java.time.LocalDateTime/now)))
   (let [conn (rmq/connect {:uri (System/getenv "RABBITMQ_URL")})
