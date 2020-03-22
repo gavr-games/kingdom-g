332c01f6c8eaf04249bd2decbb47449069794d50
andriikovalov <andrewsk@ukr.net>
2020-03-22 22:59:00 +0100

    Client test for get objects.

--- engine/test/clojure/client/api_test.cljs
+++ engine/test/clojure/client/api_test.cljs
@@ -4,6 +4,12 @@
             [engine.core :as core]
             [cljs.test :refer-macros [deftest is]]))
 
+(defn init-test-game
+  []
+  (-> (create-test-game)
+      (core/get-state-for-player 0)
+      prn-str
+      api/init-game))
 
 (deftest roundtrip-test
   (let [g (create-test-game)
@@ -11,4 +17,11 @@
         g4p-edn (prn-str g4p)]
     (api/init-game g4p-edn)
     (is (= g4p @api/game))))
+
+(deftest get-objects-test
+  (init-test-game)
+  (let [objects (js->clj (api/get-objects))
+        castle (get objects "0")]
+    (is (= 6 (count objects)))
+    (is (= "castle" (get castle "type")))))
 
